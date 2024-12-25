2102965a0ab6e21dd57bda895bf651048ef4c92e
HenriqueCarniel <henriquecarniel80@gmail.com>
2024-12-25 10:17:49 -0300

    Categories JSON

--- youtube_api.py
+++ youtube_api.py
@@ -33,11 +33,31 @@ def get_video_data(video_id):
         'published_at': snippet['publishedAt']
     }
 
+def get_video_categories():
+    request = youtube.videoCategories().list(
+        part='snippet',
+        regionCode = 'BR'
+    )
+    response = request.execute()
+
+    with open("categories.json", 'w', encoding='utf-8') as f:
+        json.dump(response, f, ensure_ascii=False, indent=4)
+
+    return response
+
 if __name__ == "__main__":
     video_id = 'dQw4w9WgXcQ'
+
+    # =================== Video data ===================
+    '''
     video_data = get_video_data(video_id)
 
     with open("rick_astley.json", 'w', encoding='utf-8') as f:
         json.dump(video_data, f, ensure_ascii=False, indent=4)
 
     print(json.dumps(video_data, indent=4, ensure_ascii=False))
+    '''
+
+    # =================== Categories ===================
+    video_categories = get_video_categories()
+    print(json.dumps(video_categories, indent=4, ensure_ascii=False))
